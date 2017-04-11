e7fbf933719af9c4d505580c299a90803bc692fd
alumno <alumno@LIMSFLAB04-13D.system-uap.adm>
2017-04-10 21:35:13 -0500

    first commit

---
+++ src/s07ejercicio09/S07ejercicio09.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s07ejercicio09;
+
+/**
+ *
+ * @author alumno
+ */
+public class S07ejercicio09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 0;
+        
+        while(true){
+            numero++;
+            System.out.println("numero"); 
+            if (numero == 100 ){
+                break;
+            }
+        }
+    }
+    
+}
